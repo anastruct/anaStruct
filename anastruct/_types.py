d5be0b56fb58d739f2d2dd28f6bb4a650f705e3f
Brooks Smith <42363318+smith120bh@users.noreply.github.com>
2026-05-30 20:52:32 +1000

    Rename types.py -> _types.py to avoid collision with builtin

---
+++ anastruct/_types.py
@@ -0,0 +1,26 @@
+from typing import TYPE_CHECKING, Dict, Literal, Sequence, TypedDict, Union
+
+import numpy as np
+
+if TYPE_CHECKING:
+    from anastruct.vertex import Vertex
+
+AxisNumber = Literal[1, 2, 3]
+Dimension = Literal["x", "y", "y_neg", "both"]
+ElementType = Literal["general", "truss"]
+LoadDirection = Literal["element", "x", "y", "parallel", "perpendicular", "angle"]
+MpType = Dict[Literal[1, 2], float]
+NumberLike = Union[float, int, np.number]
+OrientAxis = Literal["y", "z"]
+Spring = Dict[Literal[1, 2], float]
+SupportDirection = Literal["x", "y", "1", "2", 1, 2]
+VertexLike = Union[Sequence[Union[float, int]], np.ndarray, "Vertex"]
+
+SectionProps = TypedDict(
+    "SectionProps",
+    {
+        "EI": float,
+        "EA": float,
+        "g": float,
+    },
+)
